3b104b41acc04e213ce5f3a3af3eb5fd8127f8ba
Jim Allman <jim@ibang.com>
2013-04-24 00:01:29 -0400

    Basic working version of localcomments plugin

--- webapp/controllers/plugin_localcomments.py
+++ webapp/controllers/plugin_localcomments.py
@@ -1,5 +1,6 @@
 # adapted from the plugin_comments provided with web2py
 import re
+from gluon.tools import prettydate
 
 dbco = db.plugin_localcomments_comment
 
@@ -13,25 +14,27 @@ script=SCRIPT("""
 var action = null;
 var formhtml = null;
 function plugin_localcomments_init() {
-  function delete_all_forms() { jQuery('div.plugin_localcomments .reply').html(''); }
+  function delete_all_forms() { jQuery('div.plugin_localcomments div.reply').html(''); }
   function capture_form() {
-     jQuery('div.plugin_localcomments #close').click(function(){
+     jQuery('div.plugin_localcomments a.close').unbind('click').click(function(){
        delete_all_forms();
+       return false;
      });
-    jQuery('div.plugin_localcomments :submit').click(function(){
+    jQuery('div.plugin_localcomments :submit').unbind('click').click(function(){
       var $form = jQuery(this).parent();
-      jQuery.post(action+'/'+$form.parent().prev().attr('id'),
+      jQuery.post(action,
            {
-               'thread_parent_id': $form.find('input[name="thread_parent_id"]').val(),
+               ////$'thread_parent_id': form.find('input[name="thread_parent_id"]').val(),
+               'thread_parent_id': $form.parent().prev().attr('id').split('r')[1], 
                'url': $form.find('input[name="url"]').val(),
                'body': $form.find('textarea[name="body"]').val()
            },
            function(data,r){ 
                if(data) { 
                    var $refreshArea = $form.parent().next();
-                   alert($refreshArea);
                    $refreshArea.prepend(data);
                    $form.find('textarea[name="body"]').val('');
+                   //$form.find('input[name="thread_parent_id"]').val('0');
                    delete_all_forms();
                    plugin_localcomments_init(); 
                }
@@ -41,25 +44,54 @@ function plugin_localcomments_init() {
       return false;
     });
   }
-  jQuery('div.plugin_localcomments #toggle').click(function(){
-     jQuery(this).parent().next().next().slideToggle();
+  // hide unwanted toggles (for comments with no reply)
+  jQuery('div.plugin_localcomments .toggle').each(function() {
+     var $toggle = $(this);
+     var $replyHolder = $toggle.closest('li').find('ul');
+     if ($replyHolder.find('li').length === 0) {
+        $toggle.next('span').andSelf().hide();
+     } else {
+        $toggle.next('span').andSelf().show();
+     }
   });
-  jQuery('div.plugin_localcomments #reply').click(function(){
+  jQuery('div.plugin_localcomments .toggle').unbind('click').click(function(){
+     var $toggle = $(this);
+     var $replyHolder = $toggle.closest('li').find('ul').eq(0);
+     $replyHolder.slideToggle();
+     if ($toggle.text().indexOf('show') == -1) {
+        $toggle.text('show replies');
+     } else {
+        $toggle.text('hide replies');
+     }
+     return false;
+  });
+  jQuery('div.plugin_localcomments a.reply').unbind('click').click(function(){
      delete_all_forms();
-     jQuery(this).parent().next().next().slideDown();
-     jQuery(this).parent().next().html(formhtml); capture_form(); 
+     if ($(this).closest('.controls').length > 0) {
+        // this is a typical Reply link
+        $formHolder = $(this).parent().parent().parent().next();
+     } else {
+        // this is the initial 'Add a comment' link
+        $formHolder = $(this).parent().next();
+     }
+     $formFollower = $formHolder.next();
+     $formFollower.slideDown();
+     $formHolder.html(formhtml); 
+     capture_form(); 
+     return false;
   });
-  jQuery('div.plugin_localcomments #delete').click(function(){
+  jQuery('div.plugin_localcomments .delete').unbind('click').click(function(){
     delete_all_forms();
     var parent = jQuery(this).parent()
-    jQuery.post(action+'/delete/'+parent.attr('id'),null,function(data,r){parent.html('deleted');});
+    jQuery.post(action+'/delete/'+parent.attr('id'),null,function(data,r){parent.addClass('deleted').html('[deleted comment]');});
+    return false;
   });
 }
 jQuery(document).ready(function() {
   action = jQuery('div.plugin_localcomments form').attr('action');  
   formhtml = jQuery('div.plugin_localcomments form').parent().html();
-  jQuery('div.plugin_localcomments #toggle').parent().next().next().hide();
-  jQuery('div.plugin_localcomments .reply').html('');
+  ///jQuery('div.plugin_localcomments .toggle').parent().next().next().hide();
+  jQuery('div.plugin_localcomments div.reply').html('');
   plugin_localcomments_init()}
 );
 """)
@@ -69,6 +101,11 @@ def SIMPLEindex():
     return dict(comments=comments)
 
 def index():
+    # this is a tricky function that does simple display, handles POSTed comments, moderation, etc.
+
+    # TODO: break this up into more sensible functions, and refactor
+    # display/markup generation to shared code?
+
     url = request.vars['url'] # if not provided, show all
     thread_parent_id = request.vars['thread_parent_id'] # can be None
     thread = {0:[]}
@@ -76,26 +113,27 @@ def index():
         if not comment.deleted:
             #return T('COMMENT')
             return LI(
-                DIV(I('"'+comment.body+'"'),BR(),
-                    ##T('posted by %(first_name)s %(last_name)s',comment.created_by),
+                DIV(##T('posted by %(first_name)s %(last_name)s',comment.created_by),
                     # not sure why this doesn't work... db.auth record is not a mapping!?
-                    T('Posted by %s',comment.created_by.first_name),
-                    ' ', comment.created_by.last_name, 
-
-                    ' ',T('on %s',comment.created_on),' [',
-                    A(T('toggle'),_id='toggle'),
-                    '|' if auth.user_id else '',
-                    A(T('reply'),_id='reply') if auth.user_id else '',
-                    '|' if comment.created_by == auth.user_id else '',
-                    A(T('delete'),_id='delete') if comment.created_by == auth.user_id else '',
-                    ']',_id='r%s' % comment.id),
+                    DIV(comment.body,_class='body'),
+                    DIV(T('%s ',comment.created_by.first_name),T('%s',comment.created_by.last_name), T(' - %s',prettydate(comment.created_on,T)),
+                        SPAN(
+                            A(T('hide replies'),_class='toggle',_href='#'),
+                            SPAN(' | ') if auth.user_id else '',
+                            A(T('reply'),_class='reply',_href='#') if auth.user_id else '',
+                            SPAN(' | ') if comment.created_by == auth.user_id else '',
+                            A(T('delete'),_class='delete',_href='#') if comment.created_by == auth.user_id else '',
+                        _class='controls'),
+                    _class='byline'),
+                    _id='r%s' % comment.id),
                 DIV(_class='reply'),
+                # child messages (toggle hides/shows these)
                 SUL(*[node(comment) for comment in thread.get(comment.id,[])])
                 )
         elif comment.id in thread:
             return LI(
                 DIV(T('DELETED'),' [',
-                    A(T('toggle'),_id='toggle'),']'),
+                    A(T('toggle'),_class='toggle',_href='#'),']'),
                 DIV(_class='reply'),
                 SUL(*[node(comment) for comment in thread.get(comment.id,[])]))
         else: 
@@ -121,21 +159,20 @@ def index():
             return ''
         item = dbco.insert(body=request.vars.body.strip())
         return node(item)                
-    ##return DIV(T(url and ('url:'+url) or 'no url provided'))
     comments = db(dbco.url==url).select(orderby=~dbco.created_on)
-    ##return DIV(len(comments))
     for comment in comments:
         thread[comment.thread_parent_id] = thread.get(comment.thread_parent_id,[])+[comment]
     return DIV(script,
-               DIV(A(T('post'),_id='reply'),_id='r0') if auth.user_id \
-                   else A(T('login to post'),_href=URL(r=request,c='default',f='user')),
+               DIV(A(T('Add a comment'),_class='reply',_href='#'),_id='r0') if auth.user_id \
+                   else A(T('Login to add comments'),_href=URL(r=request,c='default',f='user',args=['login'])),
                DIV(FORM(TEXTAREA(_name='body',_style='width:100%; height: 40px'),
                         INPUT(_type='text',_name='url',_value=url),  # OR _value=comment.url
-                        INPUT(_type='text',_name='thread_parent_id',_value=0),   # we'll change this value on-the-fly
+                        # INPUT(_type='text',_name='thread_parent_id',_value=0),   # we'll get this from a nearby id, eg 'r8'
                         INPUT(_type='submit',_value=T('post'),_style='float:right'), 
-                        LABEL(INPUT(_type='checkbox',_value=T('claimed_expertise')), T('I claim expertise in this area'),_style='float:left; margin-right: 40px;'),
+                        LABEL(INPUT(_type='checkbox',_value=T('claimed_expertise')), T(' I claim expertise in this area'),_style='float:left; margin-right: 40px;'),
                         A(T('help'),_href='http://daringfireball.net/projects/markdown/',
                           _target='_blank',_style='float:right; padding-right: 10px'),
-                        A(T('close'),_id='close',_style='float:right; padding-right: 10px'),
+                        SPAN(' | ',_style='float:right; padding-right: 6px'),
+                        A(T('close'),_class='close',_href='#',_style='float:right; padding-right: 6px'),
                         _method='post',_action=URL(r=request,args=[])),_class='reply'),
                SUL(*[node(comment) for comment in thread[0]]),_class='plugin_localcomments')
