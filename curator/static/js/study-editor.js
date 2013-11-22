9ad6a054e5e2a09118a19b1d7fd940bfe0dece8c
Jim Allman <jim@ibang.com>
2013-11-22 14:15:23 -0500

    First run at showing D3 tree using test .json file
    This is based on earlier bare-bones tests. Note that the JSON in this case is in D3's expected format (nested node objects, each with a "children" array of more). To conserve memory, I'm hoping to teach D3 to understand a Nexson tree structure rather than re-building the tree inthis form.

--- curator/static/js/study-editor.js
+++ curator/static/js/study-editor.js
@@ -1065,13 +1065,70 @@ function showTreeViewer( tree ) {
     // quick test of modal
     $('#tree-viewer .modal-body').css({
         'height': '350px',
-        'border': '1px dashed red'
+        //'border': '1px dashed red',
+        'margin-left': '8px'
     });
     $('#tree-viewer').css({
         'width': '90%',
         'margin': 'auto -45%'
     });
-    $('#tree-viewer').modal('show')
+    $('#tree-viewer').modal('show');
+
+
+
+
+    /* load D3 tree view */
+    var width = 960,
+        height = 2200;
+
+    var cluster = d3.layout.cluster()
+        .size([height, width - 160]);
+
+    var diagonal = d3.svg.diagonal()
+        .projection(function(d) { return [d.y, d.x]; });
+
+    var svg = d3.select("#tree-viewer .modal-body").append("svg")
+        .attr("width", width)
+        .attr("height", height)
+      .append("g")
+        .attr("transform", "translate(40,0)");
+
+    d3.json("/curator/static/js/test-tree-metazoa.json", function(error, root) {
+        debugger;
+        var startTime = new Date();
+        var nodes = cluster.nodes(root),
+            links = cluster.links(nodes);
+  
+        var link = svg.selectAll(".link")
+            .data(links)
+          .enter().append("path")
+            .attr("class", "link")
+            .attr("d", diagonal);
+  
+        var node = svg.selectAll(".node")
+            .data(nodes)
+          .enter().append("g")
+            .attr("class", "node")
+            .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; })
+  
+        node.append("circle")
+            .attr("r", 4.5);
+  
+        node.append("text")
+            .attr("dx", function(d) { return d.children ? -8 : 8; })
+            .attr("dy", 3)
+            .style("text-anchor", function(d) { return d.children ? "end" : "start"; })
+            .text(function(d) { return d.name; });
+  
+        var rightNow = new Date() - startTime;
+        console.log(">> Drawing tree took "+ (rightNow / 1000.0).toFixed(2) +" seconds");
+    });
+
+
+
+
+
+
 }
 
 function filenameFromFakePath( path ) {
