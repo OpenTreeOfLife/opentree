fdd916382ee09a3207f62a8886557f41c0a375d7
Jonathan A Rees <jar398@mumble.net>
2014-01-02 13:55:01 -0500

    Remove smasher; it has moved to its own repo. Also add a README for the data directory

---
+++ data/README.md
@@ -0,0 +1,3 @@
+The data/ directory is deprecated but it is being kept for the time
+being pending review of its contents for possible salvage of code
+and/or wisdom.
--- smasher/get-ids-report.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# The wget command takes about a minute to run
-time wget -O ids_report.csv "http://reelab.net/phylografter/ottol/ottol_names_report.csv/" 
-tr "," "	" <ids_report.csv >ids_report.tab
-
-alias smash='java -classpath ".:lib/*" Smasher'
-# smash --join ids_report.tab tax/ott/deprecated.tsv >used-but-deprecated.txt 
