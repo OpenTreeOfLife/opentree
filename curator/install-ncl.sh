adbf6b3aa9fd2af386ee5122fd59fa35d1bd2f10
Mark T. Holder <mtholder@gmail.com>
2014-02-21 19:29:36 -0600

    tweaks for NCL build and rebuild

--- curator/install-ncl.sh
+++ curator/install-ncl.sh
@@ -3,6 +3,11 @@ set -x
 if ! test -d ncl
 then
     git clone https://github.com/mtholder/ncl.git || exit
+    cd ncl || exit 
+    sh bootstrap.sh || exit
+    cd ../build-ncl-static || exit
+    ../ncl/configure --enable-static --disable-shared --prefix="${NCL_INSTALL_DIR}" --with-constfuncs=yes || exit
+    cd ..
 else
     cd ncl || exit
     git pull origin master || exit
@@ -14,10 +19,7 @@ if ! test -d build-ncl-static
 then
     mkdir build-ncl-static || exit
 fi
-cd ncl/ || exit
-sh bootstrap.sh || exit
 cd ../build-ncl-static || exit
-../ncl/configure --enable-static --disable-shared --prefix="${NCL_INSTALL_DIR}" --with-constfuncs=yes || exit
 make -j2 || exit
 make check || exit
 make install || exit
