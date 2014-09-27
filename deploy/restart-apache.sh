814b61387f1ccf61bf10ab1bd97477d1e472d9cc
Jonathan A Rees <jar398@mumble.net>
2014-09-27 14:20:56 -0400

    Fix mistake in restart-apache.sh

--- deploy/restart-apache.sh
+++ deploy/restart-apache.sh
@@ -2,22 +2,21 @@
 
 OPENTREE_USER=$1
 OPENTREE_HOME=$(bash <<< "echo ~$OPENTREE_USER")
-echo "OPENTREE_HOME is $OPENTREE_HOME"
 
-if [ ! -r /etc/apache2/sites-available/opentree -o \
-     ! cmp "$OPENTREE_HOME/setup/apache-config-vhost" /etc/apache2/sites-available/opentree ]; then
+if [ ! -r /etc/apache2/sites-available/opentree ] || \
+   ! cmp "$OPENTREE_HOME/setup/apache-config-vhost" /etc/apache2/sites-available/opentree; then
     sudo cp -p "$OPENTREE_HOME/setup/apache-config-vhost" /etc/apache2/sites-available/opentree
 fi
 
-if [ ! -r /etc/apache2/sites-available/opentree-ssl -o \
-     ! cmp "$OPENTREE_HOME/setup/apache-config-vhost-ssl" /etc/apache2/sites-available/opentree-ssl ]; then
+if [ ! -r /etc/apache2/sites-available/opentree-ssl ] || \
+   ! cmp "$OPENTREE_HOME/setup/apache-config-vhost-ssl" /etc/apache2/sites-available/opentree-ssl; then
     sudo cp -p "$OPENTREE_HOME/setup/apache-config-vhost-ssl" /etc/apache2/sites-available/opentree-ssl
 fi
 
 TMP=/tmp/$$.tmp
 sed -e s+/home/opentree+$OPENTREE_HOME+ <"$OPENTREE_HOME/setup/apache-config-shared" >$TMP
-if [ ! -r /etc/apache2/opentree-config-shared -o \
-     ! cmp $TMP /etc/apache2/opentree-config-shared ]; then
+if [ ! -r /etc/apache2/opentree-config-shared ] || \
+   ! cmp $TMP /etc/apache2/opentree-config-shared; then
     sudo cp -p $TMP /etc/apache2/opentree-config-shared
 fi
 rm $TMP
