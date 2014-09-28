704a97ff8842f2db319a7d93cacb184762418ccb
Jonathan A Rees <jar398@mumble.net>
2014-09-27 22:10:58 -0400

    forgot new file

---
+++ deploy/setup/install-common.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Common setup for all web2py applications (opentree, curator, phylesystem-api)
+
+if [ "$#" -ne 0 ]; then
+    echo "install-common.sh WNA (expecting 0)"
+    exit 1
+fi
+
+. setup/functions.sh
+
+echo "Installing web2py common"
+
+bash setup/install-web2py.sh
+
+echo "...fetching opentree repo..."
+git_refresh OpenTreeOfLife opentree || true
+
+# requirements list ?
+
+(cd web2py/applications; \
+    ln -sf ../../repo/$WEBAPP/common ./)
+
+cp -p repo/opentree/oauth20_account.py web2py/gluon/contrib/login_methods/
+cp -p repo/opentree/rewrite.py web2py/gluon/
+cp -p repo/opentree/SITE.routes.py web2py/routes.py
