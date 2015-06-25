da3300e92f6f011005fb2cd262763aec68d727cc
Mark T. Holder <mtholder@gmail.com>
2015-06-25 11:55:45 +0200

    initial version of tweaking to allow deprecation messages

--- api_documentation/make_documentation.py
+++ api_documentation/make_documentation.py
@@ -1,20 +1,22 @@
 #!/usr/bin/env python
 
-from Documentation import builder
+from Documentation import Builder
 import graph, studies, taxonomy, tnrs, tree_of_life
+import sys
+d = Builder()
 
-d = builder()
-
-def print_markdown(method_group):
-    print(d.get_methods_summary(method_group))
-
+def write_markdown(out, method_group):
+    d.write_methods_summary(out, method_group)
     for m in method_group.methods_list:
-        print(d.get_detailed_methods_list(m))
+        d.write_method_details(out, m)
 
-print(d.get_doc_preamble([tree_of_life,graph,tnrs,taxonomy,studies]))
-print_markdown(tree_of_life)
-print_markdown(graph)
-print_markdown(tnrs)
-print_markdown(taxonomy)
-print_markdown(studies)
-print(d.get_doc_postamble())
+out = sys.stdout
+out.write(d.get_doc_preamble([tree_of_life,graph,tnrs,taxonomy,studies]))
+out.write('\n')
+write_markdown(out, tree_of_life)
+write_markdown(out, graph)
+write_markdown(out, tnrs)
+write_markdown(out, taxonomy)
+write_markdown(out, studies)
+out.write(d.get_doc_postamble())
+out.write('\n')
