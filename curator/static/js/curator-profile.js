176968d7a6cd4fc644d7e1417799090dfd44991d
Jim Allman <jim@ibang.com>
2015-09-26 17:23:14 -0400

    Add strict match on id for collection-list filter
    This matches only the exact (and complete) slug text in a collection id.
    N.B. however that we can match on an of
    
     - the complete collection id ('someuser/heres-my-collection')
     - just the userid slug ('someuser')
     - just the collection-name slug ('heres-my-collection')
    
    Also add a visible collection id in muted gray.

--- curator/static/js/curator-profile.js
+++ curator/static/js/curator-profile.js
@@ -197,7 +197,8 @@ function loadCollectionList(option) {
                 updateListFiltersWithHistory();
 
                 var match = viewModel.listFilters.COLLECTIONS.match(),
-                    matchPattern = new RegExp( $.trim(match), 'i' );
+                    matchPattern = new RegExp( $.trim(match), 'i' ),
+                    wholeSlugMatchPattern = new RegExp( '^'+ $.trim(match) +'$' );
                 var order = viewModel.listFilters.COLLECTIONS.order();
                 var filter = viewModel.listFilters.COLLECTIONS.filter();
 
@@ -206,8 +207,10 @@ function loadCollectionList(option) {
                     viewModel, 
                     function(collection) {
                         // match entered text against pub reference (author, title, journal name, DOI)
-                        var id, name, description, creator, contributors;
                         var id = $.trim(collection['id']);
+                        var idParts = id.split('/');
+                        var ownerSlug = idParts[0];
+                        var titleSlug = (idParts.length === 2) ? idParts[1] : '';
                         var name = $.trim(collection['name']);
                         var description = $.trim(collection['description']);
                         // extract names and IDs of all stakeholders (incl. creator!)
@@ -225,15 +228,14 @@ function loadCollectionList(option) {
                         } else {
                             contributors = "";
                         }
-
-                        if (!matchPattern.test(id) && !matchPattern.test(name) && !matchPattern.test(description) && !matchPattern.test(creator) && !matchPattern.test(contributors)) {
+                        if (!wholeSlugMatchPattern.test(id) && !wholeSlugMatchPattern.test(ownerSlug) && !wholeSlugMatchPattern.test(titleSlug) && !matchPattern.test(name) && !matchPattern.test(description) && !matchPattern.test(creator) && !matchPattern.test(contributors)) {
                             return false;
                         }
                         
                         // check for preset filters
                         switch (filter) {
                             case 'Collections they own':
-                                // show only matching studies
+                                // show only matching collections
                                 var userIsTheCreator = false;
                                 if (('creator' in collection) && ('login' in collection.creator)) { 
                                     // compare to logged-in userid provide in the main page
@@ -354,7 +356,8 @@ function loadCollectionList(option) {
 
 function getViewLink(collection) {
     // shows this collection in a popup viewer/editor
-    var html = '<a class="" href="#" title="'+ collection.id +'" onclick="fetchAndShowCollection(\''+  collection.id +'\'); return false;">'+ collection.name +'</a>';
+    var html = '<a class="" href="#" title="'+ collection.id +'" onclick="fetchAndShowCollection(\''+  collection.id +'\'); return false;">'
+        + collection.name +' <span style="color: #aaa;">&bullet;&nbsp;'+ collection.id +'</span></a>';
     return html;
 }
 function getTreeCount(collection) {
