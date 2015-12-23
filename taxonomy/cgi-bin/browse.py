c53e8bf607a9da6289faa6b938c4bec86055fe14
Jim Allman <jim@ibang.com>
2015-12-23 16:53:13 -0500

    Test restoring Content-Type "header"

--- taxonomy/cgi-bin/browse.py
+++ taxonomy/cgi-bin/browse.py
@@ -334,9 +334,9 @@ if __name__ == '__main__':
     if "name" in form: name = form["name"].value
     if "limit" in form: limit = form["limit"].value
     if "api_base" in form: api_base = form["api_base"].value
-    # Content-type information is not helpful in our current setup
-    #sys.stdout.write('Content-type: text/html\r\n')
-    #sys.stdout.write('\r\n')
+    # Content-type information is not helpful in our current setup?
+    sys.stdout.write('Content-type: text/html\r\n')
+    sys.stdout.write('\r\n')
     output = sys.stdout
     start_el(output, 'html')
     start_el(output, 'head', '')
