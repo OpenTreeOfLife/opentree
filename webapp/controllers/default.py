776a5c352473f24c23780348742b245be5134c53
Jim Allman <jim@ibang.com>
2020-06-03 18:09:46 -0400

    Try to normalize searchable feedback URLs

--- webapp/controllers/default.py
+++ webapp/controllers/default.py
@@ -23,6 +23,10 @@ def index():
     #
     # TODO: add another optional arg 'viewport', like so
     #   http://opentree.com/opentree/argus/0,23,100,400/ottol@123456/Homo+sapiens
+    try:
+        from urllib import unquote_plus
+    except ImportError:
+        from urllib.parse import unquote_plus
 
     # modify the normal view dictionary to include location+view hints from the URL
     treeview_dict = default_view_dict.copy()
@@ -34,7 +38,11 @@ def index():
     treeview_dict['nudgingToLatestSyntheticTree'] = False
     treeview_dict['incomingDomSource'] = 'none'
     treeview_dict['showLegendOnLoad'] = request.vars.get('show-legend') or False
-    treeview_dict['feedbackParentWindowURL'] = request.vars.get('parentWindowURL', None)
+    if request.vars.get('parentWindowURL', None):
+        plain_feedback_url = unquote_plus(request.vars.get('parentWindowURL'))
+        treeview_dict['feedbackParentWindowURL'] = plain_feedback_url
+    else:
+        treeview_dict['feedbackParentWindowURL'] = None
 
     # add a flag to determine whether to force the viewer to this node (vs. using the
     # browser's stored state for this URL, or a default starting node)
