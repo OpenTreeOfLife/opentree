e024ab21f9058def0b60507a1a439a50e481c20f
Jim Allman <jim@ibang.com>
2021-04-14 12:22:05 -0400

    Add sticky behavior to login-prompt link
    This will ensure that it correctly links to the Edit-study URL, even
    after adding tab/state.

--- curator/static/js/study-editor.js
+++ curator/static/js/study-editor.js
@@ -1632,7 +1632,7 @@ function updateQualityDisplay () {
                 // show appropriate text for logged-in vs anonymous user
                 var $loginToEditLink = $('a.sticky-login').eq(0);
                 editPromptHTML = 'If you want to improve this study, click <a href="#">'+ $loginToEditLink.text() +'</a> to begin.'
-                $cTabSugestionList.append('<li class="edit-prompt">'+ editPromptHTML +'</li>');
+                $cTabSugestionList.append('<li class="edit-prompt sticky-login">'+ editPromptHTML +'</li>');
                 // clicking the new link should click our smart login link
                 $cTabSugestionList.find('li.edit-prompt').unbind('click').click(function(evt) {
                     $loginToEditLink[0].click();  // call the bare DOM element for full support of onclick AND href
