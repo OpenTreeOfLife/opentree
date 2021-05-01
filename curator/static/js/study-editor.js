61b49d00fd9db9ec785d9686bdc92e4ca8f4e031
Jim Allman <jim@ibang.com>
2021-04-30 20:11:49 -0400

    Simplify UI

--- curator/static/js/study-editor.js
+++ curator/static/js/study-editor.js
@@ -5710,6 +5710,7 @@ function updateNamesetUploadForm() {
     // offer field for pasted text IF format is appropriate or unspecified
     var $pastedTextField = $('#new-nameset-text');
     
+    /*
     var chosenFormat = $.trim( $('#nameset-import-format').val() );
     switch (chosenFormat) {
         case '':
@@ -5729,14 +5730,13 @@ function updateNamesetUploadForm() {
             $pastedTextField.attr('disabled', 'disabled');
             readyToSubmit = false;
     }
+    */
 
     //var chosenFile = $.trim( $('#namesetupload').val() );
     // NO, this is routinely cleared by the upload widget; the file-name
     // display (white on blue) alongside, is actually a safer test.
-    var chosenFile = $.trim( $('#upload-nameset-info').text() );
     var pastedText = $.trim( $pastedTextField.val() );
-    // either of these is acceptable
-    if (pastedText === '' && chosenFile === '') {
+    if (pastedText === '') {
         readyToSubmit = false;
     }
 
