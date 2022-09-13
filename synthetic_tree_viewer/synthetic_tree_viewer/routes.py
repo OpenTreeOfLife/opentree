ee3bb6f8dac95da68880723a27683ec69164350a
Jim Allman <jim@ibang.com>
2022-09-13 15:16:12 -0400

    Prepare to migrate static assets and main layout

--- synthetic_tree_viewer/synthetic_tree_viewer/routes.py
+++ synthetic_tree_viewer/synthetic_tree_viewer/routes.py
@@ -1,3 +1,6 @@
 def includeme(config):
     config.add_static_view('static', 'static', cache_max_age=3600)
-    config.add_route('home', '/')
+    # match all explicit URLs before the default tree view
+
+    # default synth-tree view (with catch-all path)
+    config.add_route('tree_view', '/{full_path:.*}')
--- synthetic_tree_viewer/synthetic_tree_viewer/views/default.py
+++ synthetic_tree_viewer/synthetic_tree_viewer/views/default.py
@@ -1,6 +1,10 @@
 from pyramid.view import view_config
 
 
-@view_config(route_name='home', renderer='synthetic_tree_viewer:templates/mytemplate.jinja2')
-def my_view(request):
+@view_config(route_name='tree_view', renderer='synthetic_tree_viewer:templates/tree_view.jinja2')
+def tree_view(request):
+    #node_id = request.matchdict['node_id']
+    # examine the full path to customize this view
+    full_path = request.matchdict['full_path']
+    path_parts = full_path.split('/')
     return {'project': 'synthetic tree viewer'}
