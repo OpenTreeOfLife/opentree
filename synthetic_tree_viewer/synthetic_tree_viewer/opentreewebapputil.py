12a1004837c008820ae4f86d1b06ff4c567ec93d
Jim Allman <jim@ibang.com>
2022-10-20 00:26:43 -0400

    Migration opentreewebapputil to an internal module

---
+++ synthetic_tree_viewer/synthetic_tree_viewer/opentreewebapputil.py
@@ -0,0 +1,25 @@
+# adapted from web2py (applications.opentree.modules.opentreewebapputil)
+
+def get_user_display_name():
+    # TODO
+    pass
+
+def get_conf():
+    # TODO
+    pass
+
+def get_domain_banner_text():
+    # TODO
+    pass
+
+def get_domain_banner_hovertext():
+    # TODO
+    pass
+
+def get_currently_deployed_opentree_branch():
+    # TODO
+    pass
+
+def latest_CrossRef_URL():
+    # TODO
+    pass
--- synthetic_tree_viewer/synthetic_tree_viewer/views/default.py
+++ synthetic_tree_viewer/synthetic_tree_viewer/views/default.py
@@ -3,17 +3,19 @@ from pyramid.view import view_config
 
 @view_config(route_name='tree_view', renderer='synthetic_tree_viewer:templates/tree_view.jinja2')
 def tree_view(request):
-    from applications.opentree.modules.opentreewebapputil import get_user_display_name,
-                                                                 get_conf,
-                                                                 get_domain_banner_text,
-                                                                 get_domain_banner_hovertext,
-                                                                 get_currently_deployed_opentree_branch,
-                                                                 latest_CrossRef_URL
+    from opentreewebapputil import (
+        get_user_display_name,
+        get_conf,
+        get_domain_banner_text,
+        get_domain_banner_hovertext,
+        get_currently_deployed_opentree_branch,
+        latest_CrossRef_URL,
+        )
     #node_id = request.matchdict['node_id']
     # examine the full path to customize this view
     full_path = request.matchdict['full_path']
     path_parts = full_path.split('/')
     return {
-               'project': 'synthetic tree viewer',
-               'conf': { },  # TODO conf = get_conf(request)
-               }
+        'project': 'synthetic tree viewer',
+        'conf': { },  # TODO conf = get_conf(request)
+        }
