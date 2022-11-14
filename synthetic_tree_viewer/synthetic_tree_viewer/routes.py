4c2be3c5f054a6ccf3d3b6cb3f55b51ee1716d88
Jim Allman <jim@ibang.com>
2022-11-14 14:16:50 -0500

    Convert main layout template (til page render)

--- synthetic_tree_viewer/synthetic_tree_viewer/routes.py
+++ synthetic_tree_viewer/synthetic_tree_viewer/routes.py
@@ -1,6 +1,19 @@
 def includeme(config):
-    config.add_static_view('static', 'static', cache_max_age=3600)
+    config.add_static_view('static', 'viewer:static', cache_max_age=3600)
     # match all explicit URLs before the default tree view
+    config.add_route('home', '/')
+
+    config.add_route('about', '/about')
+    config.add_route('about_open_tree_of_life', '/about/open-tree-of-life')
+    config.add_route('about_progress', '/about/progress')
+    config.add_route('about_references', '/about/references')
+    config.add_route('about_synthesis_release', '/about/synthesis_release/{release:.*}')
+    config.add_route('about_taxonomy_version', '/about/taxonomy-version/{version:.*}')
+    config.add_route('about_developer_resources', '/about/developer-resources')
+    config.add_route('about_privacy_policy', '/about/privacy_policy')
+    config.add_route('about_licenses', '/about/licenses')
+
+    config.add_route('contact', '/contact')
 
     # default synth-tree view (with catch-all path)
     config.add_route('tree_view', '/{full_path:.*}')
--- synthetic_tree_viewer/synthetic_tree_viewer/views/default.py
+++ synthetic_tree_viewer/synthetic_tree_viewer/views/default.py
@@ -3,7 +3,7 @@ from pyramid.view import view_config
 
 @view_config(route_name='tree_view', renderer='synthetic_tree_viewer:templates/tree_view.jinja2')
 def tree_view(request):
-    from opentreewebapputil import (
+    from synthetic_tree_viewer.opentreewebapputil import (
         get_user_display_name,
         get_conf,
         get_domain_banner_text,
@@ -15,7 +15,16 @@ def tree_view(request):
     # examine the full path to customize this view
     full_path = request.matchdict['full_path']
     path_parts = full_path.split('/')
+    # provide view context for a dumb template
+    # TODO: Add boilerplate config vars (getDraftTreeID_url, etc)
     return {
-        'project': 'synthetic tree viewer',
+        'project_name': 'synthetic tree viewer',
+        #'session': request.session,
+        'response': request.response,
+        'registry': request.registry,
+        'currently_deployed_opentree_branch': 'TEST-BRANCH-NAME', # TODO get_currently_deployed_opentree_branch(request)
+        #'route_url': request.route_url,  # FUNCTIONS DON'T WORK HERE!?
+        'domain_banner_text': "TEST BANNER",  # TODO get_domain_banner_text(request)
+        'domain_banner_hovertext': "<div>Some banner details</div>",  # TODO XML(get_domain_banner_hovertext(request))
         'conf': { },  # TODO conf = get_conf(request)
         }
