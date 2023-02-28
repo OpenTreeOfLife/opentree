4a02c2d727d2f6448e16017cbb8cc8b0a41cf765
Jim Allman <jim@ibang.com>
2023-02-28 01:32:53 -0500

    Relax our blocking of new taxa with existing names
    This was previously blocked, but we want to allow for this.

--- curator/static/js/study-editor.js
+++ curator/static/js/study-editor.js
@@ -11124,10 +11124,11 @@ function taxonCondidateIsValid( candidate, options ) {
             }
             return false;
         case 'FOUND IN TAXONOMY':
+            // we want to allow existing taxon names, but let's give a warning too
             if (options.REPORT_ERRORS) {
-                showErrorMessage('There is already a taxon with this name! Homonyms are not currently allowed.');
+                showInfoMessage('There is already a taxon with this name! Proceed with caution.');
             }
-            return false;
+            // return false;
         case 'NOT FOUND':
             // it's a unique name! no problem here
             break;
