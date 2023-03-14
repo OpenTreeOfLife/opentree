1fcf08334169cd02df6150b089a9725c866c7ae0
Jim Allman <jim@ibang.com>
2023-03-14 12:44:06 -0400

    Working through alternative cases...

--- curator/static/js/study-editor.js
+++ curator/static/js/study-editor.js
@@ -2193,6 +2193,7 @@ function fetchTreeConflictStatus(inputTreeID, referenceTreeID, callback, useCach
                 return;
             }
             callback(conflictInfo);
+            updateTreeConflictWidgets(conflictInfo);
             hideModalScreen();
         }
     });
@@ -2236,19 +2237,9 @@ function fetchAndShowTreeConflictDetails(inputTreeID, referenceTreeID, options) 
             if (options.SHOW_SPINNER) {
                 hideModalScreen();
             }
-            // refresh UI (study + tree selectors) if we just opened this page?
-            var context = getPhylesystemLookupContext();
-            var $container = getPhylesystemLookupPanel( context );
-            var referenceTreeID = $container.find('.treeview-reference-select option:selected').val();
-            if (referenceTreeID === 'STUDYID_TREEID') {
-                var selectedTreeID = $container.find('.tree-lookup option:selected').val();
-                if (!selectedTreeID) {
-                    // we need to populate the selection widgets to match!
-                    console.warn("TODO: update study and tree selection widgets!");
-                } else {
-                    console.warn("NO NEED to update study + tree selectors.");
-                }
-            }
+
+            // refresh UI (study + tree selectors) if we just opened this page
+            updateTreeConflictWidgets( conflictInfo );
         },
         false  // don't reuse a cached response
     );
@@ -2322,16 +2313,17 @@ function addConflictInfoToTree( treeOrID, conflictInfo ) {
     }
 }
 
-function updateTreeConflictWidgets(conflictDetails) {
+function updateTreeConflictWidgets(conflictInfo) {
     // this should work even for incoming URLs, esp. for conflict with a published tree
     var context = getPhylesystemLookupContext();
     // what's the parent element for study+tree lookup UI?
     var $container = getPhylesystemLookupPanel( context );
 
-    var referenceTreeID = conflictDetails.referenceTreeID;
-    var referenceTreeName = conflictDetails.referenceTreeName;
+    var referenceTreeID = conflictInfo.referenceTreeID;
+    var referenceTreeName = conflictInfo.referenceTreeName;
     console.warn("referenceTreeID: "+ referenceTreeID);
     console.warn("referenceTreeName: "+ referenceTreeName);
+    console.warn(conflictInfo);
 
     //$container.find('.treeview-reference-select option:selected').val();
 
