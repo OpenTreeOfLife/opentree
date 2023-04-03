7b1e05da682447a073cd56ddf2cbc50ddd372786
Jim Allman <jim@ibang.com>
2023-04-03 16:32:12 -0400

    Link directly to reference tree in new frame.

--- curator/static/js/study-editor.js
+++ curator/static/js/study-editor.js
@@ -2336,6 +2336,7 @@ function updateTreeConflictWidgets(conflictInfo) {
             // TODO: update the study+tree selectors
             // (for now, just show a sensible footer message)
             var studyURL = getViewURLFromStudyID(studyID);
+            studyURL += ("/?tab=home&tree=" + treeID);
             showErrorMessage('Showing conflict vs. <a target="_blank" href="'+ studyURL +'">reference tree '+ treeID +' from study '+ studyID +'</a>.');
             //$container.find('.treeview-reference-select').val( 'STUDYID_TREEID' );
         } else {
