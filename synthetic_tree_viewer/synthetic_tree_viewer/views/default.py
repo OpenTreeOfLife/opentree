2f11260ee231f1dce10db2416dfc215ba126cdc5
Jim Allman <jim@ibang.com>
2023-10-10 12:11:12 -0400

    Numerous fixes for main synth-tree view

--- synthetic_tree_viewer/synthetic_tree_viewer/views/default.py
+++ synthetic_tree_viewer/synthetic_tree_viewer/views/default.py
@@ -2,6 +2,7 @@ from pyramid.view import view_config
 from synthetic_tree_viewer.opentreewebapputil import (
     get_user_display_name,
     get_conf,
+    get_conf_as_dict,
     get_domain_banner_text,
     get_domain_banner_hovertext,
     get_currently_deployed_opentree_branch,
@@ -9,30 +10,126 @@ from synthetic_tree_viewer.opentreewebapputil import (
     latest_CrossRef_URL,
     fetch_current_TNRS_context_names,
     )
+from pyramid.httpexceptions import HTTPNotFound
+
+def fetch_current_synthetic_tree_ids(request):
+    # return the latest synthetic-tree ID (and its 'life' node ID)
+    try:
+        # fetch the latest IDs as JSON from remote site
+        import json
+        import requests
+
+        method_dict = get_opentree_services_method_urls(request)
+        fetch_url = method_dict['getDraftTreeID_url']
+        if fetch_url.startswith('//'):
+            # Prepend scheme to a scheme-relative URL
+            fetch_url = "https:%s" % fetch_url
+
+        fetch_args = {}
+        # this needs to be a POST (pass fetch_args or ''); if GET, it just describes the API
+        ids_json = requests.post(url=fetch_url, data=json.dumps(fetch_args), headers={"Content-Type": "application/json"}).json()
+        draftTreeName = str(ids_json['synth_id'])
+        startNodeID = str(ids_json['root']['node_id'])
+        return (draftTreeName, startNodeID)
+
+    except Exception as e:
+        # throw 403 or 500 or just leave it
+        return ('ERROR', str(e))
+
 
 @view_config(route_name='tree_view', renderer='synthetic_tree_viewer:templates/tree_view.jinja2')
 def tree_view(request):
+    try:
+        from urllib import unquote_plus
+    except ImportError:
+        from urllib.parse import unquote_plus
+
     #node_id = request.matchdict['node_id']
     # examine the full path to customize this view
     full_path = request.matchdict['full_path']
     path_parts = full_path.split('/')
+
     # provide view context for a dumb template
+
     # First, copy our boilerplate config vars (getDraftTreeID_url, etc)
     view_dict = get_opentree_services_method_urls(request)
+
+    # retrieve latest synthetic-tree ID (and its 'life' node ID)
+    # TODO: Refresh this periodically? or only when needed for initial destination?
+    latestSyntheticTreeVersion, startingNodeID = fetch_current_synthetic_tree_ids(request)
+
     # Then add/override with these explicit key-value pairs
     view_dict.update({
         # NB - Duplicate keys will be resolved in favor of the values below!
+        'conf': get_conf(request),  # needed for the footer diagnostics
+        ##'conf_as_dict': get_conf_as_dict(request),
         'project_name': 'synthetic tree viewer',
         #'session': request.session,
         'response': request.response,
         'registry': request.registry,
         'currently_deployed_opentree_branch': get_currently_deployed_opentree_branch(request),
-        #'route_url': request.route_url,  # FUNCTIONS DON'T WORK HERE!?
+        # NB - some values will be filled in (or modified) below
+        'nodeID': '',
+        'nodeName': '',
+        'viewport': '',
         'taxonSearchContextNames': fetch_current_TNRS_context_names(request),
-        'nudgingToLatestSyntheticTree': True,  # TODO
-        'showLegendOnLoad': True,  # TODO
+        'nudgingToLatestSyntheticTree': False,
+        'forcedByURL': False,
+        'draftTreeName': latestSyntheticTreeVersion,
+        'startingNodeID': startingNodeID,
+        'incomingDomSource': 'none',
+        'showLegendOnLoad': request.params.get('show-legend') or False,
         'domain_banner_text': get_domain_banner_text(request),
         'domain_banner_hovertext': get_domain_banner_hovertext(request),
-        'conf': get_conf(request)
         })
+
+    #import pdb; pdb.set_trace()
+
+    if request.params.get('parentWindowURL', None):
+        plain_feedback_url = unquote_plus(request.params.get('parentWindowURL'))
+        view_dict['feedbackParentWindowURL'] = plain_feedback_url
+    else:
+        view_dict['feedbackParentWindowURL'] = None
+
+    # examine the path parts of the incoming URL to correctly target the view
+    ##import pdb; pdb.set_trace()
+    if len(path_parts) > 0:
+        if path_parts[0] in ['argus', 'feedback', 'properties']:  # TODO: add 'onezoom','phylet', others?
+            view_dict['viewer'] = path_parts[0]
+        elif '@' in path_parts[0]:
+            view_dict['domSource'], view_dict['nodeID'] = path_parts[0].split('@')
+        else:
+            # first arg is neither a viewer nor a proper node, which is a Bad Thing
+            raise HTTPNotFound(body="404 NOT FOUND")
+    if len(path_parts) > 1:
+        if not view_dict['nodeID']:
+        #if (not view_dict['nodeID']) and '@' in path_parts[1]:
+            ds_and_node_id = path_parts[1].split('@')
+            view_dict['domSource'] = ds_and_node_id[0]
+            if len(ds_and_node_id) > 1:
+                view_dict['nodeID'] = ds_and_node_id[1]
+        else:
+            view_dict['nodeName'] = path_parts[1]
+    if len(path_parts) > 2:
+        if not view_dict['nodeName']:
+            view_dict['nodeName'] = path_parts[2]
+
+    # replace any invalid 'domSource' (typically this is "ottol" or a synth-tree version)
+    # with the latest synthetic tree version, and notify the user on the page
+    #
+    # N.B. that if this is unspecified ('none'), the user requested a shortened
+    # URL (e.g. https://tree.opentreeoflife.org/) that resolves to the latest
+    # synthetic tree.
+    incomingDomSource = view_dict.get('domSource', None) or latestSyntheticTreeVersion
+    view_dict['incomingDomSource'] = incomingDomSource
+    if incomingDomSource not in ('ottol', latestSyntheticTreeVersion, ):
+        view_dict['domSource'] = latestSyntheticTreeVersion
+        view_dict['nudgingToLatestSyntheticTree'] = True
+        # mark this as a redirect to a different resource
+        request.response.status = 303
+
+    # when all is said and done, do we have enough information to force the location?
+    if incomingDomSource and view_dict['nodeID']:
+        view_dict['forcedByURL'] = True
+
     return view_dict
--- synthetic_tree_viewer/synthetic_tree_viewer/views/notfound.py
+++ synthetic_tree_viewer/synthetic_tree_viewer/views/notfound.py
@@ -1,7 +1,15 @@
 from pyramid.view import notfound_view_config
+from synthetic_tree_viewer.opentreewebapputil import (
+    get_conf,
+    get_conf_as_dict,
+    )
 
 
 @notfound_view_config(renderer='synthetic_tree_viewer:templates/404.jinja2')
 def notfound_view(request):
     request.response.status = 404
-    return {}
+    view_dict = {
+        'conf': get_conf(request),
+        'conf_as_dict': get_conf_as_dict(request),
+    }
+    return {'body': "404 - Not Found"}
